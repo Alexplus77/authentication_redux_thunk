90d50e8e0a79fb8711b1080b2f60627fca75bd8b
alexs <alexplus77@mail.ru>
2022-02-20 11:24:05 +0200

    commit

--- src/pages/Registration/FormRegistrationPage.jsx
+++ src/pages/Registration/FormRegistrationPage.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import "./FormRegistrationPage.css";
-import { Form, Input, Button, Checkbox } from "antd";
+import { Form, Input, Button, Typography } from "antd";
+
+const { Title } = Typography;
 const FormRegistrationPage = () => {
   const onFinish = (values) => {
     console.log("Success:", values);
@@ -10,45 +12,48 @@ const FormRegistrationPage = () => {
     console.log("Failed:", errorInfo);
   };
   return (
-    <Form
-      name="basic"
-      onFinish={onFinish}
-      onFinishFailed={onFinishFailed}
-      autoComplete="on"
-      className="form-registration"
-    >
-      <Form.Item
-        label={<label className="label">Username</label>}
-        name="username"
-        rules={[
-          {
-            required: true,
-            message: "Please input your username!",
-          },
-        ]}
+    <div className="container-form">
+      <Title>Форма регистрации пользователя</Title>
+      <Form
+        name="basic"
+        onFinish={onFinish}
+        onFinishFailed={onFinishFailed}
+        autoComplete="on"
+        className="form-registration"
       >
-        <Input />
-      </Form.Item>
+        <Form.Item
+          label={<label className="label">Username</label>}
+          name="username"
+          rules={[
+            {
+              required: true,
+              message: "Please input your username!",
+            },
+          ]}
+        >
+          <Input />
+        </Form.Item>
 
-      <Form.Item
-        label={<label className="label">Password</label>}
-        name="password"
-        rules={[
-          {
-            required: true,
-            message: "Please input your password!",
-          },
-        ]}
-      >
-        <Input.Password />
-      </Form.Item>
+        <Form.Item
+          label={<label className="label">Password</label>}
+          name="password"
+          rules={[
+            {
+              required: true,
+              message: "Please input your password!",
+            },
+          ]}
+        >
+          <Input.Password />
+        </Form.Item>
 
-      <Form.Item>
-        <Button type="primary" htmlType="submit">
-          Submit
-        </Button>
-      </Form.Item>
-    </Form>
+        <Form.Item>
+          <Button type="primary" htmlType="submit">
+            Submit
+          </Button>
+        </Form.Item>
+      </Form>
+    </div>
   );
 };
 
